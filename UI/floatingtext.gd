420fa510f0f514903ebf6a5ea69b89842c2108fb
Alex <137833578+Ceisri@users.noreply.github.com>
2023-06-30 20:07:09 +0200

    Add files via upload

---
+++ UI/floatingtext.gd
@@ -0,0 +1,12 @@
+extends Position2D
+onready var label = $Label
+onready var tween = $Label/Tween
+var amount = 0 
+func _ready():
+	label.set_text(str(amount))
+	tween.interpolate_property(self,'scale', scale , Vector2(1, 1), 0.2, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+	tween.interpolate_property(self,'scale', Vector2(1,1), Vector2(0.1,0.1), 0.7, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0.25)
+	tween.start()
+func _on_Tween_tween_all_completed():
+	self.queue_free()
+

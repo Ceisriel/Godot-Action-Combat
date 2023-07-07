2a3919867dd3bd03be5816dce3bd218933f3002d
Alex <137833578+Ceisri@users.noreply.github.com>
2023-07-08 00:00:04 +0200

    Add files via upload
    it doesn't work for multiplayer but it's the only way i got it to work.

--- scripts/Attributes.gd
+++ scripts/Attributes.gd
@@ -9,26 +9,28 @@ onready var attributelabel = $AttributePoints
 var player_script
 
 func _ready():
-	player_script = get_tree().root.get_node("Player")
+	player_script = get_tree().root.find_node("Player", true, false)
+
+
 	
 func _on_PlusVIT_pressed():
 	if attribute > 0:
 		attribute -= 1
 		attributelabel.text = "Attribute Points: " + str(attribute)
-		vitality += 0.1
+		vitality += 0.05
 		vitalitylabel.text = "Vitality: %.2f" % vitality
 
 		if player_script:
-			var maxHealth = baseHealth * vitality
+			var maxHealth = player_script.basemaxhealth * vitality
 			player_script.maxhealth = maxHealth
 			
 func _on_MinusVIT_pressed():
-	if vitality > 0.1:
+	if vitality > 0.11:
 		attribute += 1
 		attributelabel.text = "Attribute Points: " + str(attribute)
-		vitality -= 0.1
+		vitality -= 0.05
 		vitalitylabel.text = "Vitality: %.2f" % vitality
 
 		if player_script:
-			var maxHealth = baseHealth * vitality
+			var maxHealth = player_script.basemaxhealth * vitality
 			player_script.maxhealth = maxHealth

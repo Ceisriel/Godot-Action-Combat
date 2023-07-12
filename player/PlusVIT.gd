97b53ea4fe5c8ec3ad24aebd9aa6172b781ef7dd
Alex <137833578+Ceisri@users.noreply.github.com>
2023-07-13 00:04:09 +0200

    Delete player directory

--- player/PlusVIT.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-onready var playerScript = get_parent().get_node("Player")
-
-func _ready():
-	connect("pressed", self, "_onButtonPressed")
-
-func _onButtonPressed():
-	playerScript.maxhealth += 1000

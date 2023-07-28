65e4e7d70e490d94eb34ae5bfbb427e476d645fc
Alex <137833578+Ceisri@users.noreply.github.com>
2023-07-28 03:40:45 +0200

    Add files via upload

--- scripts/SaveState.gd
+++ scripts/SaveState.gd
@@ -59,12 +59,11 @@ func load_game2():
 	
 	var saved_nodes = get_tree().get_nodes_in_group("Save")
 	
-	for node in saved_nodes:
-		node.queue_free()
+
 	
 	save_file.open(save_filename,File.READ)
 	while save_file.get_position() < save_file.get_len():
 		var node_data = parse_json(save_file.get_line())
 		var new_obj = load(node_data.filename).instance()
 		get_node(node_data.parent).call_deferred('add_child',new_obj)
-		new_obj.load_save_stats(node_data)
+		saved_nodes.load_save_stats(node_data)

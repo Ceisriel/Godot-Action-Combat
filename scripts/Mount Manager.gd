d7df16feb063cbf14dd941b9bb5868fb13160755
Alex <137833578+Ceisri@users.noreply.github.com>
2023-11-22 18:52:17 +0100

    Add files via upload

--- scripts/Mount Manager.gd
+++ scripts/Mount Manager.gd
@@ -3,10 +3,10 @@ extends Control
 onready var player = $"../../.."
 onready var attachment = $"../../../FHuman/Armature/Skeleton/mount_attachment"
 onready var camera = $"../../../Camroot/Camera_holder/Camera"
-var model0: PackedScene = preload("res://Creatures/Mounts/horse/Horse1/Horse1.tscn")
-var model1: PackedScene = preload("res://Creatures/Mounts/horse/Horse2/horse2.tscn")
+var model0: PackedScene = preload("res://Creatures/Mounts/horse/Horse0/Horse0.tscn")
+var model1: PackedScene = preload("res://Creatures/Mounts/horse/Horse1/horse1.tscn")
 var model2: PackedScene = preload("res://Creatures/Mounts/horse/Horse2/Horse2.tscn")
-var model3: PackedScene = preload("res://Creatures/Mounts/horse/Horse2/horse2.tscn")
+var model3: PackedScene = preload("res://Creatures/Mounts/horse/Horse3/horse3.tscn")
 var model4: PackedScene = preload("res://Creatures/Mounts/horse/Horse4/Horse4.tscn")
 
 var currentInstance: Node = null  
@@ -32,6 +32,18 @@ func switch():
 			print("Player has horse1")
 			player.has_ride = true
 			Effect0(true)
+	elif has_model1:	
+		# Check if there is no current horse instance
+		if currentInstance == null:
+			# Instantiate the horse scene
+			currentInstance = model1.instance()
+			# Set the scale of the horse
+			currentInstance.scale *= Vector3(100, 100, 100)
+			# Add the horse as a child of the attachment node
+			attachment.add_child(currentInstance)
+			print("Player has horse1")
+			player.has_ride = true
+			Effect0(true)		
 	elif has_model2:	
 		# Check if there is no current horse instance
 		if currentInstance == null:
@@ -44,6 +56,18 @@ func switch():
 			print("Player has horse2")
 			player.has_ride = true
 			Effect0(true)			
+	elif has_model3:	
+		# Check if there is no current horse instance
+		if currentInstance == null:
+			# Instantiate the horse scene
+			currentInstance = model3.instance()
+			# Set the scale of the horse
+			currentInstance.scale *= Vector3(100, 100, 100)
+			# Add the horse as a child of the attachment node
+			attachment.add_child(currentInstance)
+			print("Player has horse3")
+			player.has_ride = true
+			Effect0(true)		
 	elif has_model4:
 		# Check if there is no current horse instance
 		if currentInstance == null:
@@ -61,7 +85,7 @@ func switch():
 		Effect0(false)
 
 func drop():
-	if has_model0:
+
 		if currentInstance != null:
 			# Remove the instanced model from the attachment
 			attachment.remove_child(currentInstance)
@@ -81,54 +105,21 @@ func drop():
 			currentInstance = null
 			player.has_ride = false
 			Effect0(false)
-	if has_model2:
-		if currentInstance != null:
-			# Remove the instanced model from the attachment
-			attachment.remove_child(currentInstance)
-			# Set the scale to 1, 1, 1
-			currentInstance.scale = Vector3(1, 1, 1)
-			# Calculate the position in front of the camera
-			# Get the global transform of the camera
-			var camera_transform = camera.global_transform
-			# Set the position of the horse in front of the camera
-			currentInstance.global_transform.origin = camera_transform.origin + camera_transform.basis.z * (-5.0)  # Adjust the distance as needed		
-			# Instance the horse back as a child of the root node
-			get_tree().root.add_child(currentInstance)
 
-			print("Horse2 dropped")
-			# Reset variables
-			has_model2 = false
-			currentInstance = null
-			player.has_ride = false
-			Effect0(false)							
-	if has_model4:
-		if currentInstance != null:
-			# Remove the instanced model from the attachment
-			attachment.remove_child(currentInstance)
-			# Set the scale to 1, 1, 1
-			currentInstance.scale = Vector3(1, 1, 1)
-			# Calculate the position in front of the camera
-			# Get the global transform of the camera
-			var camera_transform = camera.global_transform
-			# Set the position of the horse in front of the camera
-			currentInstance.global_transform.origin = camera_transform.origin + camera_transform.basis.z * (-5.0)  # Adjust the distance as needed		
-			# Instance the horse back as a child of the root node
-			get_tree().root.add_child(currentInstance)
-
-			print("Horse4 dropped")
-			# Reset variables
-			has_model4 = false
-			currentInstance = null
-			player.has_ride = false
-			Effect0(false)				
 
 func _on_ItemDetector_body_entered(body):
-	if body.is_in_group("Horse"):
+	if body.is_in_group("Horse0"):
 		if Input.is_action_pressed("E"):
 			has_model0 = true
+	elif body.is_in_group("Horse1"):
+		if Input.is_action_pressed("E"):
+			has_model1 = true			
 	elif body.is_in_group("Horse2"):
 		if Input.is_action_pressed("E"):
-			has_model2 = true			
+			has_model2 = true
+	elif body.is_in_group("Horse3"):
+		if Input.is_action_pressed("E"):
+			has_model3 = true			
 	elif body.is_in_group("Horse4"):
 		if Input.is_action_pressed("E"):
 			has_model4 = true

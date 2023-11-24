a2c90bada005113e470704a3b5ce271e50c66937
Alex <137833578+Ceisri@users.noreply.github.com>
2023-11-24 16:52:06 +0100

    Add files via upload
    added gravity and improved horse logic

--- scripts/Horse.gd
+++ scripts/Horse.gd
@@ -1,116 +1,134 @@
 extends KinematicBody
 
-var floatingtext = preload("res://UI/floatingtext.tscn")
-onready var animation = $Spearskeleton/AnimationPlayer
+
 onready var eyes = $Eyes
-onready var ray = $RayCast
-onready var hitbox = $Hitbox
-onready var namelabel = $Spatial/Viewport/Label
-onready var healthlabel = $Spatial2/Viewport/Label
-var entityname = "Skeleton Spearman"
-#timers for movement
-var directionChangeTimer = 0.0
-var directionChangeInterval = 0.0
-const minChangeInterval = 3.0
-const maxChangeInterval = 12.0
-#timers for combat 
-var switchTimer = Timer.new()
-var switchTimeMin = 1.0
-var switchTimeMax = 1.5
-#movement
-const turn = 32
-var vertical_velocity = Vector3()
-#var gravity = 30
-var state = "walk"
-var target
-# stats
+onready var IdleWalk_timer = $IdleWalk
+onready var healthlabel =$Spatial2/Viewport/HealthLabel
+onready var namelabel =$Spatial/Viewport/NameLabel
+var floatingtext = preload("res://UI/floatingtext.tscn")
+var entityname 
 var health = 100
 var maxhealth = 100
-var damage = 1
-var criticalChance = 0.70
-var criticalMultiplier = 1.5
-var criticalDefenseChance = 0.60
-var criticalDefenseMultiplier = 2
-var impact 
-var blockdamage = 3
-# artificial fps timer
-onready var fps = $Timer
-var FPS = 0.045
-#bools
-var isGrounded = false
-var blocking : bool
-var kick : bool
-var stabbing : bool
-var slashing : bool
-var slash_still : bool
-var trust: bool
-var is_blocking : bool
-#live or die
-var dead = false
-# movement speed variables
-var walkSpeed = 12.0
-var chaseSpeed = 15.0
-var fleeSpeed = 15.50
-
-# Gravity strength
-var gravity = Vector3(0, -9.8, 0)
-# Vertical speed
+var target
+var blocking = false 
+var isWalking = true  # Initial state is walking
+var alive = true
+var originalOrientation = Quat()
+var vertical_velocity = Vector3()
+var gravity = Vector3(0, -1, 0)  # Adjust the Y component as needed
 var velocity = Vector3()
+var is_grounded = false
+var ground_ray_length = 0.1  # Adjust the length of the ground check ray
 
-# Character movement speed
-var speed = 5
 
-func onhitKnockback(impact):
-	# Calculate the knockback direction
-	var knockbackDirection = -global_transform.basis.z.normalized()
-	# Apply the knockback force to the velocity
-	velocity = knockbackDirection * impact
+func _ready():
+	originalOrientation = rotation  # Store the original orientation
+	changeRandomDirection()  # Set initial random direction
+	IdleWalk_timer.connect("timeout", self, "_on_Idlewalk_timeout")
+	startIdleTimer()
 
+func startIdleTimer():
+	# Start the timer with a random timeout between 1 and 2 seconds
+	var timeout = rand_range(1, 2)
+	IdleWalk_timer.start(timeout)
+
+func _on_Artificial_physics_process_timeout():
+	
+	if not is_parent_in_player_group():
+		apply_gravity()
+		if isWalking:
+			walk()
+	else:
+		resetOrientation()  # Reset orientation if not in "World" group
+		startIdleTimer()  # Start the timer for the next state switch
+
+func walk():
+	# Walk around randomly in the direction of eyes and rotate itself to look at $Eyes
+	var speed = 5.0  # Adjust the speed as needed
+	var velocity = getSlideVelocity(speed)
+	move_and_slide(velocity)
+
+	# Check if it's time to change direction
+	if randf() < 0.01:  # You can adjust this probability as needed
+		changeRandomDirection()
+
+func idle():
+	# Stop moving
+	var velocity = Vector3.ZERO
+	move_and_slide(velocity)
+
+	# Additional idle logic if needed
+
+func changeRandomDirection():
+	var randomDirection = Vector3(rand_range(-1, 1), 0, rand_range(-1, 1)).normalized()
+	var lookRotation = randomDirection.angle_to(Vector3.FORWARD)
+	rotate_y(lookRotation)
+
+func getSlideVelocity(speed: float) -> Vector3:
+	var forwardVector = +transform.basis.z
+	return forwardVector * speed
+
+func _on_Idlewalk_timeout():
+	# Toggle between walking and idling
+	isWalking = !isWalking
+
+	if isWalking:
+		walk()
+	else:
+		idle()
+
+	# Start the timer with a new random timeout for the next state switch
+	startIdleTimer()
+func is_parent_in_player_group() -> bool:
+	# Check if the parent exists and is in the "Player" group
+	var parent = get_parent()
+	var isInPlayerGroup = parent != null and parent.is_in_group("Player")
+	
+	if isInPlayerGroup:
+		print("Parent is in the 'Player' group.")
+	
+	return isInPlayerGroup
+func resetOrientation():
+	# Reset the orientation to the original orientation when not in the "World" group
+	rotation = originalOrientation
+func _on_IdleWalk_timeout():
+	pass # Replace with function body.
+func updatehealthlabel():
+# Update health bar
+	healthlabel.text = "Health: %.2f / %.2f" % [health, maxhealth]
+# Update the UI or display a message to indicate the attribute increase
+	var healthText = "Health: %.2f / %.2f" % [health, maxhealth]	
 func takeDamage(damage):
+	updatehealthlabel()
+	if not is_parent_in_player_group():
+		if not blocking: 
+			if damage <= 0:
+				return
 
-	if not blocking: 
-		if damage <= 0:
-			return
-	# Apply critical defense chance
-		if randf() <= criticalDefenseChance:
-			damage = damage / criticalDefenseMultiplier
-
-		# Basic formula for damage
-		health -= damage
-		var text = floatingtext.instance()
-		text.amount = float(damage)
-		add_child(text)
-		if health <= 0:
-			dead = true
-		if health <= -200:
-			self.queue_free()
-	if blocking: 
-		if damage <= 0:
-			return
-	# Apply critical defense chance
-		if randf() <= criticalDefenseChance:
-			damage = damage / criticalDefenseMultiplier
-		# Basic formula for damage
-		health -= damage / blockdamage
-		var text = floatingtext.instance()
-		text.amount = float(damage)
-		add_child(text)
-	if health <= 0:
-			dead = true	
-	if health <= -200:
-			self.queue_free()
+			health -= damage
+			var text = floatingtext.instance()
+			text.amount = float(damage)
+			add_child(text)
+			if health <= 0:
+				alive = false
+			if health <= -200:
+				self.queue_free()
 
 
-func pc(delta):
-	# Apply gravity
-	velocity += gravity * delta
-	# Move the character
-	var movement = velocity * delta
-	move_and_collide(movement)
 
-	# Reset vertical speed if on the ground
+func apply_gravity():
 	if is_on_floor():
+		# If grounded, reset the Y component of velocity
 		velocity.y = 0
+	else:
+		# Apply gravity if not grounded
+		velocity += gravity 
 
+	# Check for ground contact
+	is_grounded = is_on_floor()
 
+	# Move and slide with the updated velocity
+	move_and_slide(velocity, Vector3.UP)
 
+	# Debug draw the ground check ray (optional)
+	#debug_draw_ground_ray()
